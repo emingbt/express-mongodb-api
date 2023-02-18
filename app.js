8f68cfe301972b46100ea87255ce976a9fc644a7
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2023-02-18 20:05:43 +0300

    Lint files

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-const express = require('express');
-const logger = require('morgan');
-const createError = require('http-errors');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const express = require('express')
+const logger = require('morgan')
+const createError = require('http-errors')
+const path = require('path')
+const cookieParser = require('cookie-parser')
 
-require('dotenv').config();
-require('./database-connection');
+require('dotenv').config()
+require('./database-connection')
 
-const app = express();
+const app = express()
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const authRouter = require('./routes/auth')
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 app.use('/users', usersRouter)
+app.use('/auth', authRouter)
 
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
